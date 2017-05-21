2952f990efec9b6f3657169c3b9df812cf4c46a5
carlos <carlos.pena@192.com>
2017-05-21 14:20:21 +0100

    Refactor: Use number instead of boolean

--- src/main/kotlin/org/craftycoder/tennisKata/Game.kt
+++ src/main/kotlin/org/craftycoder/tennisKata/Game.kt
@@ -2,20 +2,20 @@ package org.craftycoder.tennisKata
 
 class Game {
 
-    private var playerOneScore = false
-    private var playerTwoScore = false
+    private var playerOneScore = 0
+    private var playerTwoScore = 0
 
     fun getScore(): String =
-            if (!playerOneScore && !playerTwoScore) "Love-All"
-            else if (playerOneScore && !playerTwoScore) "Fifteen-Love"
-            else if (playerTwoScore && !playerOneScore) "Love-Fifteen"
+            if (playerOneScore == 0 && playerTwoScore == 0) "Love-All"
+            else if (playerOneScore == 1 && playerTwoScore == 0) "Fifteen-Love"
+            else if (playerTwoScore == 1 && playerOneScore == 0) "Love-Fifteen"
             else "Fifteen-All"
 
     fun addPointToPlayer1() {
-        playerOneScore = true
+        playerOneScore = 1
     }
 
     fun addPointToPlayer2() {
-        playerTwoScore = true
+        playerTwoScore = 1
     }
 }
--- src/test/kotlin/org/craftycoder/tennisKata/GameTest.kt
+++ src/test/kotlin/org/craftycoder/tennisKata/GameTest.kt
@@ -1,5 +1,6 @@
 package org.craftycoder.tennisKata
 
+import org.junit.Ignore
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -22,11 +23,18 @@ class GameTest {
         game.addPointToPlayer2()
         assertEquals("Love-Fifteen", game.getScore())
     }
-    @Test fun score_should_be_fifteen_all_when_both_player_scores_their_first_point() {
+    @Test fun score_should_be_fifteen_all_when_both_players_scores_their_first_point() {
         val game = Game()
         game.addPointToPlayer1()
         game.addPointToPlayer2()
         assertEquals("Fifteen-All", game.getScore())
+    }
+
+    @Ignore fun score_should_be_Thirty_Love_when_playerOne_scores_his_two_first_points_in_a_row() {
+        val game = Game()
+        game.addPointToPlayer1()
+        game.addPointToPlayer1()
+        assertEquals("Thirty-Love", game.getScore())
     }
 
 }
